4dd15e7acff4e17498951ec019d59c57a2aff670
Andres Santos <santfer25@hotmail.com>
2020-08-27 12:11:20 -0500

    Updates

---
+++ precache-manifest.d7518ce95df2423d2bc906033d5fbb24.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bdb5af8c2a52271086199ee632f317e",
+    "url": "/tetris-game/index.html"
+  },
+  {
+    "revision": "ff6975586c0ece058888",
+    "url": "/tetris-game/static/css/main.6d524815.chunk.css"
+  },
+  {
+    "revision": "c82487a9b7f71f766876",
+    "url": "/tetris-game/static/js/2.d1b7f841.chunk.js"
+  },
+  {
+    "revision": "eaa35b1ccc0320d7ec9314f2e19ded2a",
+    "url": "/tetris-game/static/js/2.d1b7f841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff6975586c0ece058888",
+    "url": "/tetris-game/static/js/main.d69fb4a3.chunk.js"
+  },
+  {
+    "revision": "4e54b2446b4bbe560a75",
+    "url": "/tetris-game/static/js/runtime-main.7f86b2a8.js"
+  },
+  {
+    "revision": "0b3663b83b57fb034ebf1225be945322",
+    "url": "/tetris-game/static/media/bgretro.0b3663b8.jpg"
+  },
+  {
+    "revision": "4ae628b568574cd625cfd4b67dcd1849",
+    "url": "/tetris-game/static/media/move.4ae628b5.mp3"
+  },
+  {
+    "revision": "05fbf98741ff1044332e282b03400c48",
+    "url": "/tetris-game/static/media/smallbg.05fbf987.jpg"
+  },
+  {
+    "revision": "4429a2c247c349dff0d1b2163a427c3c",
+    "url": "/tetris-game/static/media/soundgameover.4429a2c2.mp3"
+  },
+  {
+    "revision": "8238b02853756167884382a94b0d85d9",
+    "url": "/tetris-game/static/media/soundtetris.8238b028.mp3"
+  }
+]);
